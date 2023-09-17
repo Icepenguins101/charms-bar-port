5e1c0da3be0adb9804b7560296c2815ba8ff6602
Jayden Montoya <113480723+Icepenguins101@users.noreply.github.com>
2023-09-17 15:20:00 -0700

    Add files via upload

--- src/CharmsBar/App.xaml.cs
+++ src/CharmsBar/App.xaml.cs
@@ -1,13 +1,48 @@
-﻿using System;
-using System.Timers;
+﻿using NativeCode;
+using System;
+using System.IO;
 using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
+using System.Reflection;
+using System.Reflection.Metadata;
+using System.Runtime;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Runtime.InteropServices;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows;
-using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using System.Xml;
+using System.Xml.Linq;
+using Microsoft.Win32;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.DataTransfer;
+using Windows.ApplicationModel.DataTransfer.ShareTarget;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using Windows.Storage.Search;
+using Windows.Storage.Pickers;
+using Windows.Storage.Provider;
+using Windows.UI;
+using Windows.UI.Core;
+using Windows.UI.Text;
+using WinRT;
 
 namespace CharmsBarPort
 {
@@ -18,5 +53,18 @@ namespace CharmsBarPort
             System.Windows.Forms.Application.Restart();
         }
 
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            Process proc = Process.GetCurrentProcess();
+            int count = Process.GetProcesses().Where(p =>
+                p.ProcessName == proc.ProcessName).Count();
+
+            if (count > 1)
+            {
+                System.Windows.MessageBox.Show("Charms Bar Port is already running.\n\nTo close Charms Bar Port, go to the Task Manager and then end the Charms Bar Port task.");
+                App.Current.Shutdown();
+            }
         }
+
+    }
 }
